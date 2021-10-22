192e11d136026200d740846e0f3ecad559c1ea51
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-10-22 18:57:37 +0200

    grabbed and added event listeners for video controls + created basic functions

--- js/app.js
+++ js/app.js
@@ -1,3 +1,30 @@
+const videoPlayBtn = document.querySelector('.video-controls__icon--play');
+const videoPauseBtn = document.querySelector('.video-controls__icon--pause');
+const videoMute = document.querySelector('.video-controls__icon--pause');
+const videoUnMute = document.querySelector('.video-controls__icon--sound');
+
+videoPlayBtn.addEventListener('click', playVideo);
+videoPauseBtn.addEventListener('click', pauseVideo);
+videoMute.addEventListener('click', muteVideo);
+videoUnMute.addEventListener('click', unMuteVideo);
+
+function playVideo() {
+  console.log('video played');
+}
+
+function pauseVideo() {
+  console.log('video paused');
+}
+
+function muteVideo() {
+  console.log('video muted');
+}
+
+function unMuteVideo() {
+  console.log('video unmuted');
+}
+
+// ===================Swiper.js===========================
 const swiper = new Swiper('.mySwiper', {
   effect: 'cards',
   grabCursor: true,
