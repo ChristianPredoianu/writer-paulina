dd895e182f21eeabd12fb372228dd03303af995d
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-10-22 18:48:00 +0200

    added navigation, pagination on swiper

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
 const swiper = new Swiper('.mySwiper', {
   effect: 'cards',
   grabCursor: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
