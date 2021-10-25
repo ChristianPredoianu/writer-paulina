060a5b146b920b1ed792aadcf76544fc1a3b1426
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-10-25 15:17:42 +0200

    no change

--- js/app.js
+++ js/app.js
@@ -1,17 +1,37 @@
-const heroVideo = document.querySelector('.hero-container__video');
-const playControl = document.querySelector('.video-controls__icon--play');
+const heroVideo = document.querySelector('.hero__video');
 
-playControl.addEventListener('click', () => {
-  if (!heroVideo.paused) {
-    heroVideo.pause();
-    playControl.classList.remove('far', 'fa-pause-circle');
-    playControl.classList.add('far', 'fa-play-circle');
-  } else {
-    heroVideo.play();
-    playControl.classList.remove('far', 'fa-play-circle');
-    playControl.classList.add('far', 'fa-pause-circle');
-  }
-});
+function togglePlayVideo() {
+  const playControl = document.querySelector('.video-controls__icon--play');
+  playControl.addEventListener('click', () => {
+    if (!heroVideo.paused) {
+      heroVideo.pause();
+      playControl.classList.remove('far', 'fa-pause-circle');
+      playControl.classList.add('far', 'fa-play-circle');
+    } else {
+      heroVideo.play();
+      playControl.classList.remove('far', 'fa-play-circle');
+      playControl.classList.add('far', 'fa-pause-circle');
+    }
+  });
+}
+
+function toggleVideoSound() {
+  const soundControl = document.querySelector('.video-controls__icon--sound');
+  soundControl.addEventListener('click', () => {
+    if (heroVideo.muted) {
+      heroVideo.muted = false;
+      soundControl.classList.remove('fas', 'fa-volume-mute');
+      soundControl.classList.add('fas', 'fa-volume-up');
+    } else {
+      heroVideo.muted = true;
+      soundControl.classList.remove('fas', 'fa-volume-up');
+      soundControl.classList.add('fas', 'fa-volume-mute');
+    }
+  });
+}
+
+togglePlayVideo();
+toggleVideoSound();
 
 // ===================Swiper.js===========================
 const swiper = new Swiper('.mySwiper', {
