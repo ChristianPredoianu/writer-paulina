6bc39e200370e481e400b624a6037342dc6c7549
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-10-26 11:06:50 +0200

    added darkmode js

---
+++ js/darkMode.js
@@ -0,0 +1,30 @@
+const darkModeIcon = document.querySelector('.dark-mode-icon__icon');
+const body = document.querySelector('body');
+let darkMode = localStorage.getItem('darkMode');
+
+window.addEventListener('load', () => {
+  darkMode = localStorage.getItem('darkmode');
+  if (darkMode === 'true') {
+    enableDarkMode();
+  }
+});
+
+function enableDarkMode() {
+  body.classList.add('darkmode');
+  darkModeIcon.style.color = '#ffff00';
+  darkModeIcon.classList.add('dark-mode-enabled');
+  localStorage.setItem('darkmode', true);
+}
+
+function disableDarkMode() {
+  body.classList.remove('darkmode');
+  darkModeIcon.style.color = '#fff';
+  darkModeIcon.classList.remove('dark-mode-enabled');
+  localStorage.setItem('darkmode', false);
+}
+
+darkModeIcon.addEventListener('click', () => {
+  !darkModeIcon.classList.contains('dark-mode-enabled')
+    ? enableDarkMode()
+    : disableDarkMode();
+});
